c74a6f886c162d5a5edae49de61ba25687b27163
Evil Buddy <xavier.douarre@gmail.com>
2023-01-07 01:07:52 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,6 +1,5 @@
 --@shared
 
 http.get("https://raw.githubusercontent.com/evilbuddy/sf_musicdesk/main/1.0.0.lua", function(b)
-    local f = string.replace(b, "<br />")
-    loadstring(f)()
+    loadstring(b)()
 end)
