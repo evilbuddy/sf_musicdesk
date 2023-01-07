c42253c3d59575572159ade37e7ad7b3d745a66f
Evil Buddy <xavier.douarre@gmail.com>
2023-01-07 15:09:07 +0100

    1.2.0
    Added an FFT system (default gets basses)

--- latest.lua
+++ latest.lua
@@ -1,7 +1,7 @@
---@name Bureau Musical (1.1.0) | loader
+--@name Bureau Musical (1.2.0) | loader
 --@author Evil Buddy
 --@shared
 
-http.get("https://raw.githubusercontent.com/evilbuddy/sf_musicdesk/main/1.1.0.lua", function(b)
+http.get("https://raw.githubusercontent.com/evilbuddy/sf_musicdesk/main/1.2.0.lua", function(b)
     loadstring(b)()
 end)
